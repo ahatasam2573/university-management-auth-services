36c77d11650b7bcd9364689d44df78282a85b32a
unknown <ahatasamul15-3503@diu.edu.bd>
2023-06-12 10:20:27 +0600

    [DONE] Setup Logger

---
+++ src/share/logger.ts
@@ -0,0 +1,59 @@
+import { createLogger, format, transports } from 'winston'
+import DailyRotateFile from 'winston-daily-rotate-file'
+import path from 'path'
+
+const { combine, timestamp, label, printf } = format
+
+//Custom Log Format
+const myFormat = printf(({ level, message, label, timestamp }) => {
+  const date = new Date(timestamp)
+  const hour = date.getHours()
+  const minutes = date.getMinutes()
+  const seconds = date.getSeconds()
+  return `${date.toDateString()} ${hour}:${minutes}:${seconds} [${label}] ${level}: ${message}`
+})
+
+//success logger
+const logger = createLogger({
+  level: 'info',
+  format: combine(label({ label: 'SHU!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'successes',
+        'shu-%DATE%-success.log'
+      ), //for adding the path where it will save
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+//error logger
+const errorLogger = createLogger({
+  level: 'error',
+  format: combine(label({ label: 'SHU!' }), timestamp(), myFormat),
+  transports: [
+    new transports.Console(),
+    new DailyRotateFile({
+      filename: path.join(
+        process.cwd(),
+        'logs',
+        'winston',
+        'errors',
+        'shu-%DATE%-error.log'
+      ),
+      datePattern: 'YYYY-DD-MM-HH',
+      zippedArchive: true,
+      maxSize: '20m',
+      maxFiles: '14d',
+    }),
+  ],
+})
+
+export { logger, errorLogger }
